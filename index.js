49ebb8ab553475b39aedc4033e07613fcaffe6d8
Marcos Forastiere <87919057+fmarcos38@users.noreply.github.com>
2022-12-27 08:41:28 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+//--importo rutas--------------------------------------------------------
+const routeUsers = require('./src/routes/users');
+const routerAuth = require('./src/routes/auth');
+const routeCategories = require('./src/routes/categories');
+const routeProducts = require('./src/routes/products');
+const routeCompras = require('./src/routes/compras');
+const routeAddress = require('./src/routes/address');
+//-----------------------------------------------------------------------
+
+const app = express();
+dotenv.config();
+
+//--conexion DB----------------------------------------------------------
+mongoose.connect("mongodb+srv://franco:franco140183@elven.0vslzcg.mongodb.net/?retryWrites=true&w=majority", {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+})
+.then(() => console.log("mongoDB is connected"))
+.catch((err) => console.log(err));
+//-----------------------------------------------------------------------
+
+// Middleware--------------------------------
+app.use(express.json());
+app.use(cors());
+//-------------------------------------------
+
+//--invoco rutas con app-------------------------------------------------
+app.use('/users', routeUsers);
+app.use('/auth', routerAuth);
+app.use('/categories', routeCategories);
+app.use('/products', routeProducts);
+app.use('/compras', routeCompras);
+app.use('/address', routeAddress);
+//-----------------------------------------------------------------------
+
+//--pongo a escuchar/levanto servidor
+app.listen(process.env.PORT || 3001, () => {
+    console.log("Backend server is running on port 3001!");
+});
